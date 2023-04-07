43dc89d08d4fe29b70ae02c0c948f6f72bca5631
Manav Gupta <2006468@kiit.ac.in>
2023-04-07 17:30:22 +0530

    chng btn color

--- src/ui/CustomizePizza.jsx
+++ src/ui/CustomizePizza.jsx
@@ -104,7 +104,7 @@ const CustomizePizza = (props) => {
         <h1 className="text-xl font-bold">₹{customizePizzaData.price}</h1>
         <div>
           <Button
-            className="px-[15px] py-[3px] mr-2 text-[#048654] bg-white hover:text-white border-[1px] border-[#048654]"
+            className="px-4 py-1 mr-2 text-[#048654] bg-white hover:text-white border-[1px] border-[#048654]"
             onClick={props.onSetHideModal}
           >
             Cancel
